69b4ac2b02dacc239a56be4867fe69374920b196
Zaki Amin <za320@ic.ac.uk>
2021-10-14 17:19:25 +0100

    Updated semaphore to order waiter list by priority

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -50,6 +50,16 @@ sema_init (struct semaphore *sema, unsigned value)
   list_init (&sema->waiters);
 }
 
+static bool
+isThreadLowerPriority (const struct list_elem *a,
+                       const struct list_elem *b,
+                       void *aux)
+{
+  int priority1 = list_entry(a, struct thread, elem)->priority;
+  int priority2 = list_entry(b, struct thread, elem)->priority;
+  return priority1 < priority2;
+}
+
 /* Down or "P" operation on a semaphore.  Waits for SEMA's value
    to become positive and then atomically decrements it.
 
@@ -68,7 +78,7 @@ sema_down (struct semaphore *sema)
   old_level = intr_disable ();
   while (sema->value == 0) 
     {
-      list_push_back (&sema->waiters, &thread_current ()->elem);
+      list_insert_ordered (&sema->waiters, &thread_current ()->elem, isThreadLowerPriority, NULL);
       thread_block ();
     }
   sema->value--;
@@ -114,7 +124,7 @@ sema_up (struct semaphore *sema)
 
   old_level = intr_disable ();
   if (!list_empty (&sema->waiters)) 
-    thread_unblock (list_entry (list_pop_front (&sema->waiters),
+    thread_unblock (list_entry (list_pop_back (&sema->waiters),
                                 struct thread, elem));
   sema->value++;
   intr_set_level (old_level);
