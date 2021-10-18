9bba7921676010d94fb048ec3e2ea03be517210f
Dyuti Chakraborty <dyutic.home@gmail.com>
2021-10-18 15:45:07 +0100

    skeleton methods for floating point arithmetic

--- src/threads/fixed-point.c
+++ src/threads/fixed-point.c
@@ -8,3 +8,56 @@
 #include "userprog/process.h"
 #endif
 
+fp_int convert_to_fixed_point(int32_t n) 
+{
+
+}
+
+int32_t convert_to_int_round_zero(fp_int x) 
+{
+
+}
+
+int32_t convert_to_int_round_nearest(fp_int x) 
+{
+
+}
+fp_int add_fixed_points(fp_int x, fp_int y)
+{
+
+}
+
+fp_int add_fixed_point_and_integer(fp_int x, int32_t n)
+{
+
+}
+
+fp_int subtract_fixed_points(fp_int x, fp_int y)
+{
+
+}
+
+fp_int subtract_fixed_point_and_integer(fp_int x, int32_t n)
+{
+
+}
+
+fp_int multiply_fixed_points(fp_int x, fp_int y)
+{
+
+}
+
+fp_int multiply_fixed_point_and_integer(fp_int x, int32_t n)
+{
+
+}
+
+fp_int divide_fixed_points(fp_int x, fp_int y)
+{
+
+}
+
+fp_int divide_fixed_point_and_integer(fp_int x, int32_t n)
+{
+    
+}
--- src/threads/fixed-point.h
+++ src/threads/fixed-point.h
@@ -1,6 +1,25 @@
 #ifndef THREADS_FIXED_POINT_H
 #define THREADS_FIXED_POINT_H
+#include "lib/inttypes.h"
 
+#define FIXED_P (17)
+#define FIXED_Q (14)
+#define FIXED_F (1 << FIXED_Q)
 
+typedef struct fp_int {
+    int32_t val;
+} fp_int;
+
+fp_int convert_to_fixed_point(int32_t );
+int32_t convert_to_int_round_zero(fp_int );
+int32_t convert_to_int_round_nearest(fp_int );
+fp_int add_fixed_points(fp_int , fp_int );
+fp_int add_fixed_point_and_integer(fp_int , int32_t );
+fp_int subtract_fixed_points(fp_int , fp_int );
+fp_int subtract_fixed_point_and_integer(fp_int , int32_t );
+fp_int multiply_fixed_points(fp_int , fp_int );
+fp_int multiply_fixed_point_and_integer(fp_int , int32_t );
+fp_int divide_fixed_points(fp_int , fp_int );
+fp_int divide_fixed_point_and_integer(fp_int , int32_t );
 
 #endif /* threads/fixed-point.h */
