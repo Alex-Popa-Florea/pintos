bfe4ff0c9960318ac727c69d1f05b4634e18dd47
Dyuti Chakraborty <dyutic.home@gmail.com>
2021-10-18 15:01:35 +0100

    Fixed point arithmetic file skelton created

---
+++ src/threads/fixed-point.c
@@ -0,0 +1,10 @@
+#include "threads/fixed-point.h"
+#include <debug.h>
+#include <stddef.h>
+#include <random.h>
+#include <stdio.h>
+#include <string.h>
+#ifdef USERPROG
+#include "userprog/process.h"
+#endif
+
---
+++ src/threads/fixed-point.h
@@ -0,0 +1,6 @@
+#ifndef THREADS_FIXED_POINT_H
+#define THREADS_FIXED_POINT_H
+
+
+
+#endif /* threads/fixed-point.h */
