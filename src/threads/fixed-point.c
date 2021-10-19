123c0feedfd38ebd4dbc8c5bad4279469bf87294
Arunmolithevar, Thaarukan <thaarukan.arunmolithevar20@imperial.ac.uk>
2021-10-19 22:46:25 +0100

    Finish all fixed-point functions

--- src/threads/fixed-point.c
+++ src/threads/fixed-point.c
@@ -18,46 +18,46 @@ int32_t inline convert_to_int_round_zero(fp_int x)
     return (int32_t) x.val / FIXED_F;
 }
 
-int32_t convert_to_int_round_nearest(fp_int x) 
+int32_t inline convert_to_int_round_nearest(fp_int x) 
 {
-
+    return (int32_t) x.val >= 0 ? (x.val + FIXED_F/2 ) /FIXED_F : (x.val - FIXED_F/2 ) /FIXED_F;
 }
-fp_int add_fixed_points(fp_int x, fp_int y)
+fp_int inline add_fixed_points(fp_int x, fp_int y)
 {
-
-}
-
-fp_int add_fixed_point_and_integer(fp_int x, int32_t n)
-{
-
+    return (fp_int){x.val + y.val};
 }
 
-fp_int subtract_fixed_points(fp_int x, fp_int y)
+fp_int inline add_fixed_point_and_integer(fp_int x, int32_t n)
 {
-
+    return (fp_int){ x.val + n*FIXED_F};
 }
 
-fp_int subtract_fixed_point_and_integer(fp_int x, int32_t n)
+fp_int inline subtract_fixed_points(fp_int x, fp_int y)
 {
-
+    return (fp_int){x.val - y.val};
 }
 
-fp_int multiply_fixed_points(fp_int x, fp_int y)
+fp_int inline subtract_fixed_point_and_integer(fp_int x, int32_t n)
 {
-
+    return (fp_int){x.val - n*FIXED_F};
 }
 
-fp_int multiply_fixed_point_and_integer(fp_int x, int32_t n)
+fp_int inline multiply_fixed_points(fp_int x, fp_int y)
 {
-
+    return (fp_int){((int64_t) x.val) * y.val/FIXED_F};
 }
 
-fp_int divide_fixed_points(fp_int x, fp_int y)
+fp_int inline multiply_fixed_point_and_integer(fp_int x, int32_t n)
 {
-
+    return (fp_int){x.val * n};
 }
 
-fp_int divide_fixed_point_and_integer(fp_int x, int32_t n)
+fp_int inline divide_fixed_points(fp_int x, fp_int y)
 {
-    
+    return (fp_int){((int64_t) x.val) * FIXED_F/y.val};
+}
+
+fp_int inline divide_fixed_point_and_integer(fp_int x, int32_t n)
+{
+    return (fp_int){x.val/n};
 }
