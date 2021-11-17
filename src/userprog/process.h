f074ed9cff00c8d3fd426946973a751c24cbc255
Zaki Amin <za320@ic.ac.uk>
2021-11-17 20:54:36 +0000

    za320 aap120 dc420 ta620 Fix system call exec to block a process until child has loaded

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -20,35 +20,44 @@ void verify_address (const void *vaddr);
     Struct for a process control block, storing a process and relevant info
 */
 typedef struct {   
-    int id;                         // Id of the thread which equals the id of the process
-    int parent_id;                  // A parent_id of -1 means a process has no parent
-    int waiting_on;                 // Id of the child process that a parent process waits on
-    struct list_elem elem;          // Elem to insert pcb into a list
-    int exit_status;                // Status of the process
-    struct semaphore sema;          // Semaphore used to block the thread of the corresponding process
-    bool has_been_waited_on;        // Boolean to track if a wait has already been called on the process
+    int id;                         /* Id of the thread which equals the id of the process */
+    int parent_id;                  /* A parent_id of -1 means a process has no parent */
+    struct list_elem elem;          /* Elem to insert pcb into a list */
+    int exit_status;                /* Status of the process */
+
+    int waiting_on;                 /* Id of the child process that a parent process waits on */
+    bool has_been_waited_on;        /* Boolean to track if a wait has already been called on the process */
+    struct semaphore wait_sema;     /* Semaphore used to block a process when it waits on its child process */
+
+    bool load_process_success;      /* Flag to indicate success of loading a process from an executable */
+    struct semaphore load_sema;     /* Semaphore to block a process while it loads a child */
+    int child_load_status;          /* 0: default, 1: waiting for child to load, 2: child has loaded */
 } pcb;
+
 
 /*
     Initialises a PCB with its id and parent id
 */
 void init_pcb (pcb *, int, int);
 
+
 /*
     Returns true if the process has another process as its child
 */
-bool
-process_has_child (pcb *parent, pid_t child_id);
+bool process_has_child (pcb *parent, pid_t child_id);
+
 
 /*
     Sets the id of the child process a parent waits on
 */
 void pcb_set_waiting_on (pcb *, int);
 
+
 /*
-    Returns the PCB corresponding to an id, NULL if there is no match
+    Returns a pointer to the pcb corresponding to an id, NULL if there is no match
 */
 pcb *get_pcb_from_id (tid_t);
+
 
 void print_pcb_ids (void);
 
